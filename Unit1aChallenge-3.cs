e3eb42b34be0b6946578973f680bd37228c469db
Elias <epic00eli@gmail.com>
2023-09-07 15:31:40 -0600

    Created a program that asks a math question.
    Prompts user if answer correct or incorrect.

---
+++ Unit1aChallenge-3.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+  int x = 15;
+  int y = 15;
+  Console.WriteLine("If x = " + x + " and y = " + y);
+  Console.WriteLine("Then what is z in: x + y = z?");
+  int z = Convert.ToInt32(Console.ReadLine());
+  Console.WriteLine("Your answer is: " + z);
+  bool answer = z == x + y;
+  if (answer)
+  {
+      Console.WriteLine("Correct!");
+  }
+  else
+  {
+      Console.WriteLine("Incorrect, please try again");
+  }
+    }
+}
