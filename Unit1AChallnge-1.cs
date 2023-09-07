42c7d84a115759d06ad69a38d585bea098d7bfb2
Elias <epic00eli@gmail.com>
2023-09-07 13:19:31 -0600

    Created a program that displays my name and values of x and y.

---
+++ Unit1AChallnge-1.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+     string name = "Elias";
+     Console.WriteLine("Hi my name is " + name);
+     int x = 15;
+     Console.WriteLine("x = " + x);
+     bool result = x > 30;
+     Console.WriteLine("Is x > 30? " + result);
+     double y = 13.545D;
+     Console.WriteLine("y = " + y);
+     }
+  }
+}
