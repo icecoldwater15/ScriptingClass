0e9136e1cdc56920626f0d4cd0ee35bd91ed9c11
Elias <epic00eli@gmail.com>
2023-09-14 14:19:46 -0600

    Expanding grading program

--- Unit1/Unit1b/Unit1bLabChallenge2.cs
+++ Unit1/Unit1b/Unit1bLabChallenge2.cs
@@ -3,29 +3,36 @@ using System;
 public class Unit1b
 {
      public static void Main(string[] args)
-     {
-        Console.WriteLine ("What was your score, as a percent?");
-        int score = Convert.ToInt32(Console.ReadLine());
-        string grade = "your grade is ";
-        if (score >= 90)
+    {
+        Console.WriteLine ("What subject would like to submit a grade for?");
+        string subject = Console.ReadLine();
+        switch (subject)
         {
-            Console.WriteLine (grade + 'A');
+            case Math:
+                Console.WriteLine ("What was your score, as a percent?");
+                int score = Convert.ToInt32(Console.ReadLine());
+                string grade = "your grade is ";
+                if (score >= 90)
+                {
+                    Console.WriteLine (grade + 'A');
+                }
+                else if (score >= 80)
+                {
+                    Console.WriteLine (grade + 'B');
+                }
+                else if (score >= 70)
+                {
+                    Console.WriteLine (grade + 'C');
+                }
+                else if (score >= 60)
+                {
+                    Console.WriteLine (grade + 'D');
+                }
+                else if (score < 60)
+                {
+                    Console.WriteLine (grade + 'F');
+                }
+                break;
         }
-        else if (score >= 80)
-        {
-            Console.WriteLine (grade + 'B');
-        }
-        else if (score >= 70)
-        {
-            Console.WriteLine (grade + 'C');
-        }
-        else if (score >= 60)
-        {
-            Console.WriteLine (grade + 'D');
-        }
-        else if (score < 60)
-        {
-            Console.WriteLine (grade + 'F');
-        }
-     }
+    }
 }
