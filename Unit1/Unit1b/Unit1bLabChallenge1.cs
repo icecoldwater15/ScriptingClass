fea5cee8ad0344d30a5ac33f4594b1fbd6fd0c4d
Elias <epic00eli@gmail.com>
2023-09-14 13:48:57 -0600

    Enhanced weather program to read more temperatures.

--- Unit1/Unit1b/Unit1bLabChallenge1.cs
+++ Unit1/Unit1b/Unit1bLabChallenge1.cs
@@ -6,13 +6,21 @@ public class Unit1b
     {
         Console.WriteLine ("What is the temperature in celsius right now?");
         int temperature = Convert.ToInt32(Console.ReadLine());
-        if (temperature > 30)
-        {
-            Console.WriteLine ("Wow, " + temperature + " degrees is pretty hot, make sure you stay hydrated today!");
-        }
-        else
-        {
-            Console.WriteLine ("Only " + temperature + " degrees? Enjoy the pleasent weather today!");
-        }
+       if (temperature >= 30)
+       {
+        Console.WriteLine ("Pretty hot outside, I would suggest light clothing and staying well hydrated today.");
+       }
+       else if (temperature >= 20)
+       {
+        Console.WriteLine ("Not too bad outside but still a little chilly. Perfect for a sweatshirt.");
+       }
+       else if (temperature >= 10)
+       {
+        Console.WriteLine ("Seems pretty cold out right now, Make sure to wear a jacket.");
+       }
+       else if (temperature >= 0)
+       {
+        Console.WriteLine ("Seems very cold outside right now, I recommend staying inside and keeping warm.");
+       }
     }
 }
