5188c2e0d497eb3432c818eb2de79c9d570a8f91
Elias <epic00eli@gmail.com>
2023-09-27 19:08:35 -0600

    Created movement script

---
+++ Unit3/Unit3Lab/Unit3Lab/Assets/Script/PlayerController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 15f;
+    [SerializeField] private float jumpForce = 500f;
+    [SerializeField] private float gravity = 15f;
+
+    private CharacterController controller;
+    [SerializeField] private Vector3 moveDirection;
+    [SerializeField] private bool isJumping;
+    
+    [SerializeField] private float horizontalInput;
+    [SerializeField] private float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        moveDirection = new Vector3(horizontalInput, 0, verticalInput);
+        moveDirection *= moveSpeed;
+        moveDirection.y -= gravity;
+
+        if (Input.GetButtonDown("Jump") && controller.isGrounded) {
+            isJumping = true;
+        } else {
+            isJumping = false;
+        }
+
+        if (isJumping && controller.isGrounded) {
+            moveDirection.y = jumpForce;
+            isJumping = false;
+        }
+
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
