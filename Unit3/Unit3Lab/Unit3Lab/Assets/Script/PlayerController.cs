18ae93834cb184c1d4c79564bd614c82f86bed5b
Elias <epic00eli@gmail.com>
2023-09-28 21:22:18 -0600

    Created better player controls.

--- Unit3/Unit3Lab/Unit3Lab/Assets/Script/PlayerController.cs
+++ Unit3/Unit3Lab/Unit3Lab/Assets/Script/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 15f;
+    [SerializeField] private float moveSpeed = 10f;
     [SerializeField] private float jumpForce = 500f;
     [SerializeField] private float gravity = 15f;
 
@@ -13,46 +13,52 @@ public class NewBehaviourScript : MonoBehaviour
     [SerializeField] private bool isJumping;
     [SerializeField] private float horizontalInput;
     [SerializeField] private float verticalInput;
-
+    [SerializeField] private bool isCrouching;
+    [SerializeField] private Vector2 faceDirection;
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        faceDirection.x += Input.GetAxis("Mouse X");
+        faceDirection.y += Input.GetAxis("Mouse Y");
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
         moveDirection = new Vector3(horizontalInput, 0, verticalInput);
+        moveDirection = transform.TransformDirection(moveDirection);
         moveDirection *= moveSpeed;
         moveDirection.y -= gravity;
-
-        if (Input.GetButtonDown("Sprint") && controller.isGrounded)
+        transform.localRotation = Quaternion.Euler(-faceDirection.y, faceDirection.x, 0);
+        if (Input.GetKeyDown(KeyCode.LeftShift) && controller.isGrounded)
         {
             moveSpeed = 25f;
         }
-        else
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
         {
-
+            moveSpeed = 10f;
         }
-        
-        if (Input.GetButtonDown("Jump") && controller.isGrounded) 
+        if (Input.GetKeyDown(KeyCode.LeftControl))
         {
-            isJumping = true;
-        } 
-        else 
+            isCrouching = true;
+            transform.localScale = new Vector3(1f,0.5f,1f);
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftControl))
         {
-            isJumping = false;
+            isCrouching = false;
+            transform.localScale = new Vector3(1f,1f,1f);
         }
 
-        if (isJumping && controller.isGrounded) 
+        if (Input.GetButtonDown("Jump") && controller.isGrounded && isCrouching == false) 
         {
             moveDirection.y = jumpForce;
-            isJumping = false;
-        }
+        } 
 
+        
         controller.Move(moveDirection * Time.deltaTime);
     }
 }
